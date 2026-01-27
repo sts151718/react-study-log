2bbc32d680d033b31f939a6587901b30416d92ac
satoshi-tsubokura <tsubokurajob151718@gmail.com>
2026-01-27 22:23:25 +0900

    テストデータを一覧で表示

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,19 @@
 export const App = () => {
-  return <h1>学習記録一覧</h1>
-}
+  const records = [
+    { title: '勉強の記録1', time: 1 },
+    { title: '勉強の記録2', time: 3 },
+    { title: '勉強の記録3', time: 5 },
+  ];
+  const test = 'a';
+
+  return (
+    <>
+      <h1>学習記録一覧</h1>
+      {records.map((rec) => (
+        <p>
+          {rec.title} {rec.time}時間
+        </p>
+      ))}
+    </>
+  );
+};
